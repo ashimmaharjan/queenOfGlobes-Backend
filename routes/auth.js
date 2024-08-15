85287cce1d9ec5013eec13b8daa27dc87c4d9c8a
Ashim Maharjan <ciferulquiorra008@gmail.com>
2024-08-15 20:40:41 +1000

    Session added to user logins

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,11 @@ const User = require("../models/User");
 // Register a new user
 router.post("/signup", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
-
   try {
     const newUser = new User({ firstName, lastName, email, password });
     await newUser.save();
-    res.status(201).json(newUser);
+    req.session.userId = newUser._id;
+    res.status(201).json({ userId: newUser._id });
     console.log("New user created successfully.");
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -25,7 +25,8 @@ router.post("/login", async (req, res) => {
     if (!user || user.password !== password) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    res.status(200).json(user);
+    req.session.userId = user._id;
+    res.status(200).json({ userId: user._id });
     console.log("Login Successful.");
   } catch (error) {
     res.status(500).json({ error: error.message });
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const authRoutes = require("./routes/auth");
 
 dotenv.config();
@@ -18,6 +20,17 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
+// Set up session management
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+  })
+);
 
 app.use("/api/auth", authRoutes);
 
